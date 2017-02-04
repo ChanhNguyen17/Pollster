58b6ad88e14fa7f770358e3daac28af863b2aac9
Chanh Nguyen <chanhnguyen@Chanh-Nguyens-iMac.local>
2017-02-04 23:33:14 +0200

    Add QandA

---
+++ Pollster/CloudQandATableViewController.swift
@@ -0,0 +1,40 @@
+//
+//  CloudQandATableViewController.swift
+//  Pollster
+//
+//  Created by Chanh Nguyen on 2/4/17.
+//  Copyright © 2017 Stanford University. All rights reserved.
+//
+
+import UIKit
+import CloudKit
+
+class CloudQandATableViewController: QandATableViewController {
+    
+    var ckQandARecord: CKRecord {
+        get {
+            if _ckQandARecord == nil {
+                _ckQandARecord = CKRecord(recordType: Cloud.Entity.QandA)
+            }
+            return _ckQandARecord!
+        }
+        set {
+            _ckQandARecord = newValue
+        }
+    }
+    
+    private var _ckQandARecord: CKRecord? {
+        didSet {
+            let question = ckQandARecord[Cloud.Attribute.Question] as? String ?? ""
+            let answers = ckQandARecord[Cloud.Attribute.Answers] as? [String] ?? []
+            qanda = QandA(question: question, answers: answers)
+            
+            asking = ckQandARecord.wasCreatedByThisUser
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        ckQandARecord = CKRecord(recordType: Cloud.Entity.QandA)
+    }
+}
